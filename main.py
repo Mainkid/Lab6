c4b8deb91be8c75ad08da548934034e0ad4c304a
AlekseyK <Alekov99@mail.ru>
2020-11-13 15:51:47 +0500

    updated

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 import integralCalculation
 
-
-def main():
-    integralCalculation.trapeze_method()
-    integralCalculation.trapeze_method_modified()
-
-
-if __name__ == '__main__':
-    main()
-
+integralCalculation.trapeze_method()
+integralCalculation.trapeze_method_modified()
